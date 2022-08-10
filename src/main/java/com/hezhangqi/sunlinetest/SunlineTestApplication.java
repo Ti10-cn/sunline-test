83ed81b6e839281256b674ca8cc6fc7027e65cd2
Ti-cn <Ti-cn@psglgd.onmicrosoft.com>
2022-08-10 10:30:06 +0800

    new

---
+++ src/main/java/com/hezhangqi/sunlinetest/SunlineTestApplication.java
@@ -0,0 +1,13 @@
+package com.hezhangqi.sunlinetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunlineTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunlineTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hezhangqi/sunlinetest/SunlineTestApplicationTests.java
@@ -0,0 +1,14 @@
+package com.hezhangqi.sunlinetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunlineTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
