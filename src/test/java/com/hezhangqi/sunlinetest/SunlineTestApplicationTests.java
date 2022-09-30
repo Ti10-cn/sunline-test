06bedafbdabdd7357a7a47202914bae776dff4af
Ti-cn <Ti-cn@psglgd.onmicrosoft.com>
2022-09-30 17:59:13 +0800

    20220930

--- src/test/java/com/hezhangqi/sunlinetest/SunlineTestApplicationTests.java
+++ src/test/java/com/hezhangqi/sunlinetest/SunlineTestApplicationTests.java
@@ -5,15 +5,20 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
+
 @SpringBootTest
 class SunlineTestApplicationTests {
     @Autowired
     private Redhat001 redhat001;
 
+
     @Test
     void contextLoads() {
-       int i = redhat001.findByName();
+       int i = 1;
         System.out.println(i);
+        BigDecimal bigDecimal01 = new BigDecimal("0.00000000000000000123");
+        System.out.println(bigDecimal01);
     }
 
 }
